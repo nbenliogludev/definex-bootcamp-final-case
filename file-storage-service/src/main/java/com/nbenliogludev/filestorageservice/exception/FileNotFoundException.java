36cfb11cf8ba47d4ac278705a9788bfef3efe99f
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-09 18:04:51 +0300

    Add FileNotFoundException and InvalidFileFormatException to file-storage-service.

---
+++ file-storage-service/src/main/java/com/nbenliogludev/filestorageservice/exception/FileNotFoundException.java
@@ -0,0 +1,11 @@
+package com.nbenliogludev.filestorageservice.exception;
+
+/**
+ * @author nbenliogludev
+ */
+public class FileNotFoundException extends RuntimeException {
+    public FileNotFoundException() {
+        super("FileNotFoundException");
+    }
+}
+
---
+++ file-storage-service/src/main/java/com/nbenliogludev/filestorageservice/exception/InvalidFileFormatException.java
@@ -0,0 +1,11 @@
+package com.nbenliogludev.filestorageservice.exception;
+
+/**
+ * @author nbenliogludev
+ */
+public class InvalidFileFormatException extends RuntimeException {
+    public InvalidFileFormatException() {
+        super("InvalidFileFormatException");
+    }
+}
+
