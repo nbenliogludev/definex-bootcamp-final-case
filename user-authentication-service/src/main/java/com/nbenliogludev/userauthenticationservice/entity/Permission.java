b5782d0c724fe096da71175432b34f8926fd082d
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-12 20:38:51 +0300

    feat: add core entity classes (Permission, Role, Token, TokenType, User) to user-authentication-service.

---
+++ user-authentication-service/src/main/java/com/nbenliogludev/userauthenticationservice/entity/Permission.java
@@ -0,0 +1,25 @@
+package com.nbenliogludev.userauthenticationservice.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author nbenliogludev
+ */
+@RequiredArgsConstructor
+public enum Permission {
+
+    ADMIN_READ("admin:read"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_DELETE("admin:delete"),
+    MANAGER_READ("management:read"),
+    MANAGER_UPDATE("management:update"),
+    MANAGER_CREATE("management:create"),
+    MANAGER_DELETE("management:delete")
+
+    ;
+
+    @Getter
+    private final String permission;
+}
