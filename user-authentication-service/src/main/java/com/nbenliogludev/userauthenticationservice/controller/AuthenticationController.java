e2e2ab108e8bf2d9f46dfc46e2fbbc1141ed16a8
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-12 20:57:20 +0300

    Add AuthenticationController.

---
+++ user-authentication-service/src/main/java/com/nbenliogludev/userauthenticationservice/controller/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.nbenliogludev.userauthenticationservice.controller;
+
+import com.nbenliogludev.userauthenticationservice.dto.request.AuthenticationRequestDTO;
+import com.nbenliogludev.userauthenticationservice.dto.request.UserCreateRequestDTO;
+import com.nbenliogludev.userauthenticationservice.dto.response.AuthenticationResponseDTO;
+import com.nbenliogludev.userauthenticationservice.dto.response.RestResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<RestResponse<AuthenticationResponseDTO>> register(
+            @RequestBody UserCreateRequestDTO request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<RestResponse<AuthenticationResponseDTO>> authenticate(
+            @RequestBody AuthenticationRequestDTO request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+}
