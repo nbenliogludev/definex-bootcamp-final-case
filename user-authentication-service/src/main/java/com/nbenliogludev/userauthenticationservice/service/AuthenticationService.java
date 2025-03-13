5174db6dedd7850772ba4f7c31ce632f0c0024eb
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-13 19:25:44 +0300

    Add AuthenticationService and JwtService to user-authentication-service.

---
+++ user-authentication-service/src/main/java/com/nbenliogludev/userauthenticationservice/service/AuthenticationService.java
@@ -0,0 +1,54 @@
+package com.nbenliogludev.userauthenticationservice.service;
+
+import com.nbenliogludev.userauthenticationservice.dto.request.UserCreateRequestDTO;
+import com.nbenliogludev.userauthenticationservice.dto.response.AuthenticationResponseDTO;
+import com.nbenliogludev.userauthenticationservice.entity.Role;
+import com.nbenliogludev.userauthenticationservice.entity.Token;
+import com.nbenliogludev.userauthenticationservice.entity.TokenType;
+import com.nbenliogludev.userauthenticationservice.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final TokenRepository tokenRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponseDTO register(UserCreateRequestDTO request) {
+        User user = User.builder()
+                .firstname(request.name())
+                .lastname(request.surname())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .role(Role.USER)
+                .build();
+
+        User savedUser = repository.save(user);
+
+        var jwtToken = jwtService.generateTokenWithUserId(user, savedUser.getId());
+        var refreshToken = jwtService.generateRefreshToken(user);
+        saveUserToken(savedUser, jwtToken);
+        return AuthenticationResponseDTO.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .id(savedUser.getId()) // Include userId in response
+                .build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+}
