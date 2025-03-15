c7a40e3f08fbb7fbbd2a7018111a7488a9d76240
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-15 14:53:08 +0300

    Add Dockerfile for Eureka, enable discovery client, update docker-compose.

--- file-storage-service/src/main/java/com/nbenliogludev/filestorageservice/FileStorageServiceApplication.java
+++ file-storage-service/src/main/java/com/nbenliogludev/filestorageservice/FileStorageServiceApplication.java
@@ -2,8 +2,10 @@ package com.nbenliogludev.filestorageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FileStorageServiceApplication {
 
 	public static void main(String[] args) {
