95fb78a93e94f9579662b31cd43d3cf90e061248
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-18 15:44:23 +0300

    Add Comment entity to task-management-service.

---
+++ task-management-service/src/main/java/com/nbenliogludev/taskmanagementservice/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.nbenliogludev.taskmanagementservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+/**
+ * @author nbenliogludev
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+}
