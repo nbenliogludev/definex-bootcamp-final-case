e8f0c0a9ee4f17a07afa52aaf5298299c5accb4e
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-20 14:45:49 +0300

    Add createTask endpoint to task-management-service.

--- task-management-service/src/main/java/com/nbenliogludev/taskmanagementservice/controller/TaskManagementController.java
+++ task-management-service/src/main/java/com/nbenliogludev/taskmanagementservice/controller/TaskManagementController.java
@@ -1,7 +1,9 @@
 package com.nbenliogludev.taskmanagementservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @RequiredArgsConstructor
 public class TaskManagementController {
+
+    private final TaskService taskService;
+
+    @PostMapping("/v1")
+    public ResponseEntity<RestResponse<TaskCreateRequestDTO>> createTask(@RequestBody TaskDto taskDto) {
+        return ResponseEntity.ok(RestResponse.of(taskService.createTask(taskDto)));
+    }
+
 }
