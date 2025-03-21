03048aa1500faafcb40f95c693beb71a792fe677
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-21 22:30:55 +0300

    Add ProjectController to taks-management-service.

---
+++ task-management-service/src/main/java/com/nbenliogludev/taskmanagementservice/controller/ProjectController.java
@@ -0,0 +1,30 @@
+package com.nbenliogludev.taskmanagementservice.controller;
+
+import com.nbenliogludev.taskmanagementservice.dto.response.RestResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author nbenliogludev
+ */
+@RestController
+@RequestMapping("/api/task-management/projects")
+@Validated
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @PostMapping("/v1")
+    public ResponseEntity<RestResponse<ProjectCreateResponseDTO>> createProject(@RequestBody ProjectCreateRequestDTO projectDto) {
+        return ResponseEntity.ok(RestResponse.of(projectService.createProject(projectDto)));
+    }
+
+    @GetMapping
+    public ResponseEntity<RestResponse<List<ProjectCreateResponseDTO>>> getAllProjects() {
+        return ResponseEntity.ok(RestResponse.of(projectService.getAllProjects()));
+    }
+}
