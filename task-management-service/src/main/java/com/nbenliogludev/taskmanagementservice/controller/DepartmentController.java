e3e69e8f5d09ab5f3fb353bb04e230191998599b
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-21 22:27:48 +0300

    Add getAllDepartments to DepartmentController in task-management-service.

--- task-management-service/src/main/java/com/nbenliogludev/taskmanagementservice/controller/DepartmentController.java
+++ task-management-service/src/main/java/com/nbenliogludev/taskmanagementservice/controller/DepartmentController.java
@@ -7,10 +7,9 @@ import com.nbenliogludev.taskmanagementservice.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author nbenliogludev
@@ -26,6 +25,11 @@ public class DepartmentController {
     @PostMapping("/v1")
     public ResponseEntity<RestResponse<DepartmentCreateResponseDTO>> createDepartment(@RequestBody DepartmentCreateRequestDTO depaptmentDto) {
         return ResponseEntity.ok(RestResponse.of(departmentService.createDepartment(depaptmentDto)));
+    }
+
+    @GetMapping
+    public ResponseEntity<RestResponse<List<DepartmentCreateResponseDTO>>> getAllDepartments() {
+        return ResponseEntity.ok(RestResponse.of(departmentService.getAllDepartments()));
     }
 
 }
