685a6596ecd201245e44df00083b0df1460557bb
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2025-03-22 19:32:36 +0300

    Modify ProjectServiceImpl.

--- task-management-service/src/main/java/com/nbenliogludev/taskmanagementservice/service/impl/ProjectServiceImpl.java
+++ task-management-service/src/main/java/com/nbenliogludev/taskmanagementservice/service/impl/ProjectServiceImpl.java
@@ -2,7 +2,10 @@ package com.nbenliogludev.taskmanagementservice.service.impl;
 
 import com.nbenliogludev.taskmanagementservice.dto.request.ProjectCreateRequestDTO;
 import com.nbenliogludev.taskmanagementservice.dto.response.ProjectCreateResponseDTO;
+import com.nbenliogludev.taskmanagementservice.entity.Department;
 import com.nbenliogludev.taskmanagementservice.entity.Project;
+import com.nbenliogludev.taskmanagementservice.mapper.ProjectMapper;
+import com.nbenliogludev.taskmanagementservice.repository.DepartmentRepository;
 import com.nbenliogludev.taskmanagementservice.repository.ProjectRepository;
 import com.nbenliogludev.taskmanagementservice.service.ProjectService;
 import lombok.RequiredArgsConstructor;
@@ -13,20 +16,24 @@ import java.util.List;
 /**
  * @author nbenliogludev
  */
+
 @Service
 @RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
+    private final DepartmentRepository departmentRepository;
     private final ProjectMapper projectMapper;
 
     @Override
     public ProjectCreateResponseDTO createProject(ProjectCreateRequestDTO request) {
-        Project project = new Project();
-        project.setName(request.name());
-        Project saved = projectRepository.save(project);
+        Project project = projectMapper.mapToProject(request);
+        Department department = departmentRepository.findById(request.departmentId())
+                .orElseThrow(() -> new IllegalArgumentException("Department not found"));
 
-        return new ProjectCreateResponseDTO(saved.getId(), saved.getName());
+        project.setDepartment(department);
+        Project saved = projectRepository.save(project);
+        return projectMapper.mapToProjectResponse(saved);
     }
 
     @Override
